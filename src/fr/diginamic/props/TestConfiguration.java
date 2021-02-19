dfd120470d4a3d33f5c2cb9b1262bbee1419d381
AntoninB4440 <barceloantonin@gmail.com>
2021-02-19 10:52:31 +0100

    ajout boucle

--- src/fr/diginamic/props/TestConfiguration.java
+++ src/fr/diginamic/props/TestConfiguration.java
@@ -9,6 +9,13 @@ public class TestConfiguration {
 
 		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
 
+		/*
+		 * Boucle sur le fichier Enumeration<String> keys = monFichierConf.getKeys();
+		 * 
+		 * while (keys.hasMoreElements()) { String key = keys.nextElement();
+		 * System.out.println(monFichierConf.getString(key)); }
+		 */
+
 		String driverName = monFichierConf.getString("database.driver");
 
 		System.out.println(driverName);
