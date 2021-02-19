5e5fa1cf1525d3ef82145cf585d48fc8d3082a43
AntoninB4440 <barceloantonin@gmail.com>
2021-02-19 10:10:36 +0100

    TP1 config file

---
+++ src/fr/diginamic/props/TestConfiguration.java
@@ -0,0 +1,17 @@
+package fr.diginamic.props;
+
+import java.util.ResourceBundle;
+
+public class TestConfiguration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
+
+		String driverName = monFichierConf.getString("database.driver");
+
+		System.out.println(driverName);
+	}
+
+}
